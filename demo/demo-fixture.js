ab4b84f98b2b1ff546e2734b41afe04a47c1f15e
Trent Brown <trent.brown@mx.com>
2020-12-16 19:59:27 -0800

    WIP

--- demo/demo-fixture.js
+++ demo/demo-fixture.js
@@ -26,7 +26,6 @@ An element that accepts username and password to connect to a document source.
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import 'ft-element-demo/ft-element-demo.js';
 
 import '@polymer/iron-flex-layout/iron-flex-layout-classes.js';
 import '@polymer/paper-button/paper-button.js';
